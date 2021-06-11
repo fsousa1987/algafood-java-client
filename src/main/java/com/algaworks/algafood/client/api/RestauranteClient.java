2385ba9caa35c22c5cd1dbf4507ecec19d966eaf
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-11 12:59:03 -0300

    Implementando uma requisição POST no client Java

--- src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
+++ src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood.client.api;
 
+import com.algaworks.algafood.client.model.RestauranteModel;
 import com.algaworks.algafood.client.model.RestauranteResumoModel;
+import com.algaworks.algafood.client.model.input.RestauranteInput;
 import lombok.AllArgsConstructor;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 public class RestauranteClient {
 
-    private static final String RESOURCE_PATH = "/restaurantesss";
+    private static final String RESOURCE_PATH = "/restaurantes";
 
     private final RestTemplate restTemplate;
     private final String url;
@@ -28,6 +31,18 @@ public class RestauranteClient {
             return Arrays.asList(restaurantes);
         }
         catch (RestClientResponseException e) {
+            throw new ClientApiException(e.getMessage(), e);
+        }
+    }
+
+    public RestauranteModel adicionar(RestauranteInput restaurante) {
+        var resourceUri = URI.create(url + RESOURCE_PATH);
+
+        try {
+            return restTemplate
+                    .postForObject(resourceUri, restaurante, RestauranteModel.class);
+        }
+        catch (HttpClientErrorException e) {
             throw new ClientApiException(e.getMessage(), e);
         }
     }
